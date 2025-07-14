ec4b158f94b87002ce6520f8b1197cd6f539dde8
vibhav30 <guptavibhav123@gmail.com>
2025-07-14 23:33:17 +0530

    Deploy

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,14 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+const _dirname=path.resolve();
 
 // middleware
 app.use(express.json());
@@ -23,16 +27,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
-
-
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (_, res)=>{
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT,()=>{
     connectDB();
--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -5,7 +5,7 @@ import { getAdminJobs, getAllJobs, getJobById, postJob, deleteJob } from "../con
 const router = express.Router();
 
 router.route("/post").post(isAuthenticated, postJob);
-router.route("/get").get(isAuthenticated, getAllJobs);
+router.route("/get").get(getAllJobs);
 router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
 router.route("/get/:id").get(getJobById);
 router.route("/delete/:id").delete(isAuthenticated, deleteJob);
